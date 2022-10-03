00e9db6df0c8a944863ae44b5eef98ca98d15bcc
EIDRE <eidreema5@gmail.com>
2022-10-03 21:42:03 +0300

    تسجيل دخول

--- app/src/main/java/com/example/afinal/ActivityStart.java
+++ app/src/main/java/com/example/afinal/ActivityStart.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
 import android.widget.Button;
 
 public class ActivityStart extends AppCompatActivity {
@@ -25,7 +24,8 @@ public class ActivityStart extends AppCompatActivity {
             @Override
             public void run() {
                 // This method will be executed once the timer is over
-                Intent i = new Intent(ActivityStart.this, ActivityShow.class);
+                Intent i = new Intent(ActivityStart.this, SignUp.class);
+
                 startActivity(i);
                 finish();
             }
--- app/src/main/java/com/example/afinal/sighnin.java
+++ app/src/main/java/com/example/afinal/sighnin.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class EmailpassActivity extends AppCompatActivity {
+public class sighnin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.emailpass);
+        setContentView(R.layout.activity_sighnin);
     }
 }
